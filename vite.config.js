6bc802b1eae37394e8703f081125b67fe26aef7e
Minh_Đức <ducle090891999@gmail.com>
2025-02-02 16:22:56 +0700

    h

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 3000, // Render sẽ cung cấp cổng thông qua biến môi trường PORT
-  },
+    port: process.env.PORT || 3000
+  },  
   build: {
     rollupOptions: {
       output: {
